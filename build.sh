77fbb3b473b1740dcb6696f38a42eae80aa69486
Rodrigo Agurto <12665665+ro-ag@users.noreply.github.com>
2022-04-28 02:57:33 -0400

    centos stream

--- build.sh
+++ build.sh
@@ -1,8 +1,11 @@
-docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_VER=8  -t rodagurto/perl:5.34.1-centos.8 .
-docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_VER=7  -t rodagurto/perl:5.34.1-centos.7 .
+docker build --build-arg PERL_VERSION=5.34.1 -t rodagurto/perl:5.34.1-centos.stream .
+docker build --build-arg PERL_VERSION=5.34.1 -t rodagurto/perl:5.34.1-centos.latest .
 
-docker build --build-arg PERL_VERSION=5.32.1 --build-arg DOCKER_VER=8  -t rodagurto/perl:5.32.1-centos.8 .
-docker build --build-arg PERL_VERSION=5.32.1 --build-arg DOCKER_VER=7  -t rodagurto/perl:5.32.1-centos.7 .
+docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=8  -t rodagurto/perl:5.34.1-centos.8 .
+docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=7  -t rodagurto/perl:5.34.1-centos.7 .
 
-docker build --build-arg PERL_VERSION=5.30.3 --build-arg DOCKER_VER=8  -t rodagurto/perl:5.30.3-centos.8 .
-docker build --build-arg PERL_VERSION=5.30.3 --build-arg DOCKER_VER=7  -t rodagurto/perl:5.30.3-centos.7 .
+docker build --build-arg PERL_VERSION=5.32.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=8  -t rodagurto/perl:5.32.1-centos.8 .
+docker build --build-arg PERL_VERSION=5.32.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=7  -t rodagurto/perl:5.32.1-centos.7 .
+
+docker build --build-arg PERL_VERSION=5.30.3 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=8  -t rodagurto/perl:5.30.3-centos.8 .
+docker build --build-arg PERL_VERSION=5.30.3 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=7  -t rodagurto/perl:5.30.3-centos.7 .
