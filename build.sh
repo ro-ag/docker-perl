ba585d4c77a0515f735627cdf5b2d432962a4ea2
Rodrigo Agurto <12665665+ro-ag@users.noreply.github.com>
2022-04-28 04:02:58 -0400

    fix tag

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build --build-arg PERL_VERSION=5.34.1 -t rodagurto/perl:5.34.1-centos.stream .
-docker build --build-arg PERL_VERSION=5.34.1 -t rodagurto/perl:5.34.1-centos.latest .
+docker build --build-arg PERL_VERSION=5.34.1 -t rodagurto/perl:latest .
 
 docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=8  -t rodagurto/perl:5.34.1-centos.8 .
 docker build --build-arg PERL_VERSION=5.34.1 --build-arg DOCKER_IMG=centos --build-arg DOCKER_VER=7  -t rodagurto/perl:5.34.1-centos.7 .
